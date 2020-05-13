3a4522d4986f735958fc8a5856a79d622e42d5e5
Vagoom <v.malickis@mail.ru>
2020-05-13 19:08:55 +0300

    vadim changed welcome msg

--- guess.php
+++ guess.php
@@ -8,7 +8,7 @@ $GUESS_TO   = (int) ($argv[2] ?? 1000);
 $DEBUG      = strtoupper($argv[3] ?? '') === 'DEBUG';
 
 println();
-println("********** Welcome to the number guesser! **********");
+println("********** Welcome to the Vadim's guesser! **********");
 println();
 println(sprintf("Debug mode is %s", $DEBUG ? "ON" : "OFF"));
 println();
